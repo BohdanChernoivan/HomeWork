ad56dc3bb08d31c1be623c6d5ddba76e13a7852e
GodDAN <scoliztur@gmail.com>
2019-02-12 02:03:53 +0200

    callback in DragonRide

--- Dz_4_5/src/bohdan/homework/com/Fighters/Riders/DragonRider.java
+++ Dz_4_5/src/bohdan/homework/com/Fighters/Riders/DragonRider.java
@@ -1,12 +1,13 @@
 package bohdan.homework.com.Fighters.Riders;
 
 
+import bohdan.homework.com.Fighters.Base.Actions.FightBreakAction;
 import bohdan.homework.com.Fighters.Base.ArenaFighter;
 import bohdan.homework.com.Fighters.Dragons.Dragon;
-import bohdan.homework.com.Fighters.Riders.DragonPet.DragonPet;
 
 
-public class DragonRider extends ArenaFighter implements DragonPet{ //TODO: not over yet
+
+public class DragonRider extends ArenaFighter implements FightBreakAction {
 
     private Dragon dragonPet = null;
 
@@ -14,38 +15,22 @@ public class DragonRider extends ArenaFighter implements DragonPet{ //TODO: not 
         super(name, health, damage, defense);
     }
 
-
-    public void dragonPET(Dragon dragon) {
-            dragonPet = dragon;
+    @Override
+    public void actionWithFight(ArenaFighter arenaFighter, FightCallBack fightCallBack) {
+        if (arenaFighter instanceof Dragon) {
+            dragonPet = (Dragon) arenaFighter;
             setDamage(dragonPet.getDamage());
+            fightCallBack.fightImmediatelyDone(this);
+        }
+    }
+
+    @Override
+    public int setReturnDamage(int damage) {
+        return super.setReturnDamage(damage + dragonPet.getFullDamage());
     }
 }
 
 
-
-
-/*    public void countHealth() {
-        setHealth(getHealth() + dragonPet.getHealth());
-    }
-
-    public void countDamage() {
-        setDamage(getDamage() + dragonPet.getDamage());
-    }
-
-    private boolean isDragonAlive() {
-        if (dragonPet.getHealth() >= dragonPet.getFullHP()) {
-            return true;
-        } else return false;
-    }
-    */
-
-// if(arenaFighter instanceof Dragon)
-//    public void doActionBeforeFight(ArenaFighter arenaFighter) {
-//        if (arenaFighter instanceof Dragon) {
-//            dragonPet = (Dragon) arenaFighter;
-//        }
-//        super.damageArenaFighter(arenaFighter);
-//    }
 
 
 
