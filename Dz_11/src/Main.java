c32386280f1d5d3af42379d638728a0dd6577029
GodDAN <scoliztur@gmail.com>
2019-03-05 20:40:14 +0200

    DataFormat, Dz_11

--- Dz_11/src/Main.java
+++ Dz_11/src/Main.java
@@ -2,17 +2,28 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
         Calendar calendar = new GregorianCalendar();
 
-        SimpleDateFormat data = new SimpleDateFormat("yyyy/MMMM/dd");
+        SimpleDateFormat formatOne = new SimpleDateFormat("yyyy-MMMM-dd");
+        SimpleDateFormat formatTwo = new SimpleDateFormat("E" + ", d MMM yyyy HH:mm:ss Z");
+        SimpleDateFormat formatThree = new SimpleDateFormat("h:mm a" , Locale.ENGLISH);
+        SimpleDateFormat formatFour = new SimpleDateFormat("MM'/'dd'/'y hh:mm");
+        SimpleDateFormat formatFive = new SimpleDateFormat("zzzz -> dd MMMM yyyy");
+        SimpleDateFormat formatSix = new SimpleDateFormat("yyyy.MMMM.dd GGG hh:mm aaa", Locale.ENGLISH);
 
-        String s = String.format("Today's date:\n%s", data.format(calendar.getTime()));
+        String str = "Today's date:\n%s\n\n";
 
-        System.out.println(s);
+        System.out.printf(str, formatOne.format(calendar.getTime()).toUpperCase());
+        System.out.printf(str, formatTwo.format(calendar.getTime()));
+        System.out.printf(str, formatThree.format(calendar.getTime()));
+        System.out.printf(str, formatFour.format(calendar.getTime()));
+        System.out.printf(str, formatFive.format(calendar.getTime()));
+        System.out.printf(str, formatSix.format(calendar.getTime()));
 
     }
 }
